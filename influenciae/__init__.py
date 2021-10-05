853f7a6e735cd799f4beb2986b1f1d91fb4516ef
Thomas FEL <thomas.fel@protonmail.com>
2021-10-05 16:19:34 +0200

    influenciae: introduce base structure

---
+++ influenciae/__init__.py
@@ -0,0 +1,12 @@
+"""
+Influenciae
+-------
+
+The goal of Influenciae is to provide a simple interface to the various influence functions
+techniques
+"""
+
+__version__ = '0.0.1'
+
+from . import influence
+from . import common
