1970b7e6243c62f06085d3322d48b2bdda8efd87
Thomas FEL <thomas_fel@brown.edu>
2021-11-27 17:18:28 +0100

    tests: introduce commons utility functions

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,34 @@
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
 
 def almost_equal(arr1, arr2, epsilon=1e-6):
     """Ensure two array are almost equal at an epsilon"""
     return np.sum(np.abs(arr1 - arr2)) < epsilon
+
+
+def assert_tensor_equal(tensor1, tensor2):
+    return tf.debugging.assert_equal(tensor1, tensor2)
+
+
+def generate_data(x_shape=(32, 32, 3), num_labels=10, samples=100):
+    x = np.random.rand(samples, *x_shape).astype(np.float32)
+    y = to_categorical(np.random.randint(0, num_labels, samples), num_labels)
+
+    return x, y
+
+
+def generate_model(input_shape=(32, 32, 3), output_shape=10):
+    model = Sequential()
+    model.add(Input(shape=input_shape))
+    model.add(Conv2D(4, kernel_size=(2, 2),
+                     activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(20))
+    model.add(Dense(output_shape))
+    model.compile(loss='categorical_crossentropy', optimizer='sgd')
+
+    return model
