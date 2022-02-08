9456a4aacc3678084ce24c87c17a8ee8edde95c7
Thomas FEL <thomas.fel@protonmail.com>
2022-02-08 21:45:08 +0100

    influence: move cardinality equality assertion into a helper

--- influenciae/common/__init__.py
+++ influenciae/common/__init__.py
@@ -2,5 +2,5 @@
 Utiliy classes and functions
 """
 
-from .tf_operations import find_layer, is_dataset_batched, assert_batched_dataset
+from .tf_operations import find_layer, is_dataset_batched, assert_batched_dataset, dataset_size
 from .model_wrappers import InfluenceModel
