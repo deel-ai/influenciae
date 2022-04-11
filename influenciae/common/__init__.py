8be63a3d4e3f00de8afb93867955b35b03396ded
agustin-martin.picard <agustin-martin.picard@irt-saintexupery.com>
2022-04-11 13:44:44 +0200

    Implement a conjugate gradient descent solver to compute IHVPs later

--- influenciae/common/__init__.py
+++ influenciae/common/__init__.py
@@ -1,6 +1,7 @@
 """
-Utiliy classes and functions
+Utility classes and functions
 """
 
 from .tf_operations import find_layer, is_dataset_batched, assert_batched_dataset, dataset_size
 from .model_wrappers import InfluenceModel
+from .conjugate_gradients import conjugate_gradients_solve
