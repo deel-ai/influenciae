b2fef041087793fdd82df6539eddecc38706223f
Lucas Hervier <lucas.hervier@irt-saintexupery.com>
2022-04-13 10:09:10 +0200

    copyright: add copyright header to all python script and licence

--- tests/common/test_compute_hessian_block.py
+++ tests/common/test_compute_hessian_block.py
@@ -1,3 +1,7 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
 import pytest
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Dense
