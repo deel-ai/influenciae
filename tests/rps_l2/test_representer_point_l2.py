64b7ae85ee16096281cf0c27b0fa26fd0f3d5b16
agustin-martin.picard <agustin-martin.picard@irt-saintexupery.com>
2022-08-24 12:03:16 +0200

    test rps_l2: reduce the size of the models and implement the test for the predictions from kernel

--- tests/rps_l2/test_representer_point_l2.py
+++ tests/rps_l2/test_representer_point_l2.py
@@ -11,14 +11,10 @@ from ..utils import almost_equal
 def test_surrogate_model():
     x_train = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
     y_train = tf.keras.utils.to_categorical(tf.random.categorical(tf.math.log([[0.25, 0.25, 0.25, 0.25]]), 100))
-    x_test = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
-    y_test = tf.keras.utils.to_categorical(tf.random.categorical(tf.math.log([[0.25, 0.25, 0.25, 0.25]]), 100))
     train_set = tf.data.Dataset.from_tensor_slices((x_train, tf.cast(tf.squeeze(y_train), tf.float32)))
-    test_set = tf.data.Dataset.from_tensor_slices((x_test, tf.cast(tf.squeeze(y_test), tf.float32)))
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(32, 32, 3)),
-        tf.keras.layers.Conv2D(16, 3, 2, "same", activation='swish'),
-        tf.keras.layers.Conv2D(32, 3, 2, "same", activation='swish'),
+        tf.keras.layers.Conv2D(16, 3, 4, "same", activation='swish'),
         tf.keras.layers.GlobalAveragePooling2D(),
         tf.keras.layers.Dense(4)
     ])
@@ -26,7 +22,7 @@ def test_surrogate_model():
         optimizer=tf.keras.optimizers.Adam(1e-2),
         loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
         metrics=['accuracy'])
-    model.fit(train_set.shuffle(100).batch(32), epochs=40, validation_data=test_set.batch(32), verbose=0)
+    model.fit(train_set.shuffle(100).batch(32), epochs=40, verbose=0)
     rps_l2 = RepresenterPointL2(model, train_set.batch(32), lambda_regularization=0.1)
 
     # Check the shapes of the surrogate model
@@ -39,21 +35,17 @@ def test_surrogate_model():
     surrogate_model = rps_l2.linear_layer
     original_preds = model(x_train)
     surrogate_preds = surrogate_model(rps_l2.feature_extractor(x_train))
-    mean_mse = tf.reduce_mean(tf.keras.losses.mse(original_preds, surrogate_preds))
-    assert mean_mse < 1e-1
+    mean_bce = tf.reduce_mean(tf.keras.losses.binary_crossentropy(original_preds, surrogate_preds))
+    assert mean_bce < 0.1
 
 
 def test_gradients():
     x_train = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
     y_train = tf.random.categorical(tf.math.log([[0.5, 0.5]]), 100)
-    x_test = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
-    y_test = tf.random.categorical(tf.math.log([[0.5, 0.5]]), 100)
     train_set = tf.data.Dataset.from_tensor_slices((x_train, tf.cast(tf.squeeze(y_train), tf.float32)))
-    test_set = tf.data.Dataset.from_tensor_slices((x_test, tf.cast(tf.squeeze(y_test), tf.float32)))
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(32, 32, 3)),
-        tf.keras.layers.Conv2D(16, 3, 2, "same", activation='relu'),
-        tf.keras.layers.Conv2D(32, 3, 2, "same", activation='relu'),
+        tf.keras.layers.Conv2D(16, 3, 4, "valid", activation='relu'),
         tf.keras.layers.GlobalAveragePooling2D(),
         tf.keras.layers.Dense(1)
     ])
@@ -63,7 +55,7 @@ def test_gradients():
                                                 reduction=tf.keras.losses.Reduction.NONE),
         metrics=['accuracy']
     )
-    model.fit(train_set.shuffle(100).batch(32), epochs=40, validation_data=test_set.batch(32), verbose=0)
+    model.fit(train_set.shuffle(100).batch(32), epochs=40, verbose=0)
     rps_l2 = RepresenterPointL2(model, train_set.batch(32), lambda_regularization=0.1)
     rps_l2._train_last_layer()
     surrogate_model = rps_l2.linear_layer
@@ -79,14 +71,10 @@ def test_gradients():
 def test_influence_values():
     x_train = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
     y_train = tf.random.categorical(tf.math.log([[0.5, 0.5]]), 100)
-    x_test = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
-    y_test = tf.random.categorical(tf.math.log([[0.5, 0.5]]), 100)
     train_set = tf.data.Dataset.from_tensor_slices((x_train, tf.cast(tf.squeeze(y_train), tf.float32)))
-    test_set = tf.data.Dataset.from_tensor_slices((x_test, tf.cast(tf.squeeze(y_test), tf.float32)))
     model = tf.keras.Sequential([
         tf.keras.layers.Input(shape=(32, 32, 3)),
-        tf.keras.layers.Conv2D(16, 3, 2, "same", activation='swish'),
-        tf.keras.layers.Conv2D(32, 3, 2, "same", activation='swish'),
+        tf.keras.layers.Conv2D(16, 3, 5, "valid", activation='relu'),
         tf.keras.layers.GlobalAveragePooling2D(),
         tf.keras.layers.Dense(1)
     ])
@@ -96,7 +84,7 @@ def test_influence_values():
                                                 reduction=tf.keras.losses.Reduction.NONE),
         metrics=['accuracy']
     )
-    model.fit(train_set.shuffle(100).batch(32), epochs=40, validation_data=test_set.batch(32), verbose=0)
+    model.fit(train_set.shuffle(100).batch(32), epochs=40, verbose=0)
     lambda_regularization = 0.1
     rps_l2 = RepresenterPointL2(model, train_set.batch(20), lambda_regularization=lambda_regularization)
     rps_l2._train_last_layer()
@@ -114,4 +102,28 @@ def test_influence_values():
 
 
 def test_predict_with_kernel():
-    pass
+    x_train = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
+    y_train = tf.random.categorical(tf.math.log([[0.5, 0.5]]), 100)
+    x_test = tf.random.normal((100, 32, 32, 3), dtype=tf.float32)
+    y_test = tf.random.categorical(tf.math.log([[0.5, 0.5]]), 100)
+    train_set = tf.data.Dataset.from_tensor_slices((x_train, tf.cast(tf.squeeze(y_train), tf.float32)))
+    test_set = tf.data.Dataset.from_tensor_slices((x_test, tf.cast(tf.squeeze(y_test), tf.float32)))
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(32, 32, 3)),
+        tf.keras.layers.Conv2D(16, 3, 5, "valid", activation='relu'),
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(1e-2),
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True,
+                                                reduction=tf.keras.losses.Reduction.NONE),
+        metrics=['accuracy']
+    )
+    model.fit(train_set.shuffle(100).batch(32), epochs=40, validation_data=test_set.batch(32), verbose=0)
+    lambda_regularization = 10.
+    rps_l2 = RepresenterPointL2(model, train_set.batch(20), lambda_regularization=lambda_regularization)
+    kernel_preds = rps_l2.predict_with_kernel(train_set.map(lambda x, y: x).batch(20))
+    model_preds = model.predict(train_set.map(lambda x, y: x).batch(20))
+    bce_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True, reduction=tf.keras.losses.Reduction.SUM)
+    assert bce_loss(tf.squeeze(model_preds), kernel_preds) / 100. < 0.1
