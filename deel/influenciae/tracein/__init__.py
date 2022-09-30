f3f2f07591080d56112a780b9690e0fd5187efc2
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2022-09-30 18:12:41 +0200

    tracin: fix linting errors

---
+++ deel/influenciae/tracein/__init__.py
@@ -0,0 +1,8 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+"""
+TraceIn Module
+"""
+from .tracin import TracIn
