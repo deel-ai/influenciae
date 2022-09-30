b4eccaf339aa15a0b07e19883ffa6f70c8855565
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2022-09-30 18:12:08 +0200

    rps: change rps_lje to match the paper

--- deel/influenciae/rps/rps_lje.py
+++ deel/influenciae/rps/rps_lje.py
@@ -2,16 +2,22 @@
 # rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
 # CRIAQ and ANITI - https://www.deel.ai/
 # =====================================================================================
+"""
+TODO
+"""
+import itertools
+
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential # pylint: disable=E0611
 
-from ..common import InfluenceModel, VectorBasedInfluenceCalculator
-from ..common import InverseHessianVectorProduct
+from ..common import InfluenceModel
+from ..common import InverseHessianVectorProductFactory
 
-from ..utils import from_layer_name_to_layer_idx
-from ..types import Union, Tuple
+from ..influence import FirstOrderInfluenceCalculator
+from ..types import Union, Optional
 
-class RPSLJE(VectorBasedInfluenceCalculator):
+
+class RPSLJE(FirstOrderInfluenceCalculator):
     """
     Representer Point Selection via Local Jacobian Expansion for Post-hoc Classifier Explanation of Deep Neural
     Networks and Ensemble Models
@@ -30,90 +36,80 @@ class RPSLJE(VectorBasedInfluenceCalculator):
 
     def __init__(
             self,
-            model: InfluenceModel,
-            ihvp_calculator: InverseHessianVectorProduct,
-            target_layer: Union[int, str]
+            model_influence: InfluenceModel,
+            dataset: tf.data.Dataset,
+            ihvp_calculator_factory: InverseHessianVectorProductFactory,
+            n_samples_for_hessian: Optional[int] = None,
+            target_layer: Union[int, str] = -1,
+            shuffle_buffer_size: Optional[int] = 10000
     ):
-        self.model = model
-        self.ihvp_calculator = ihvp_calculator
-        if isinstance(target_layer, str):
-            target_layer = from_layer_name_to_layer_idx(model, target_layer)
+        ihvp_calculator = ihvp_calculator_factory.build(model_influence, dataset)
+        first_order_calculator = FirstOrderInfluenceCalculator(model=model_influence,
+                                                               dataset=dataset,
+                                                               ihvp_calculator=ihvp_calculator,
+                                                               n_samples_for_hessian=n_samples_for_hessian,
+                                                               shuffle_buffer_size=shuffle_buffer_size,
+                                                               normalize=False)
 
-        self.feature_model = Sequential(self.model.layers[:target_layer])
+        influence_vector_dataset = first_order_calculator.compute_influence_vector_dataset(dataset)
 
-    def compute_influence_vector(self, train_samples: Tuple[tf.Tensor, tf.Tensor]) -> tf.Tensor:
-        """
-        Compute the influence vector for a training sample
+        size = tf.data.experimental.cardinality(dataset)
+        iter_dataset = iter(influence_vector_dataset)
 
-        Parameters
-        ----------
-        train_samples
-            sample to evaluate
-        Returns
-        -------
-        The influence vector for the training sample
-        """
-        # batch_size = tf.shape(train_samples[0])[0]
+        weight_size = tf.reduce_sum(
+            tf.stack([tf.reduce_prod(tf.shape(w)) for w in model_influence.model.layers[target_layer].weights]))
 
-        # TODO - improve: API IHVP shall accept tensor
-        ihvp = self.ihvp_calculator.compute_ihvp_single_batch(train_samples)
+        def body(i, v, nb):
+            current_vector = next(iter_dataset)[1]
 
-        vec_weight = tf.concat([tf.reshape(w, (1, -1)) for w in self.model.weights], axis=1)
-        vec_weight = tf.repeat(vec_weight, tf.shape(ihvp)[1], axis=0)
-        #TODO: ask questions here
-        ihvp = tf.cast(vec_weight, dtype=ihvp.dtype) - tf.transpose(ihvp)
+            nb_next = nb + tf.cast(tf.shape(current_vector)[0], dtype=nb.dtype)
 
-        return ihvp
+            v_current = tf.reduce_sum(current_vector, axis=0)
 
-    def preprocess_sample_to_evaluate(self, samples_to_evaluate: Tuple[tf.Tensor, tf.Tensor]) -> tf.Tensor:
-        """
-        Preprocess a sample to evaluate
+            v_next = (nb / nb_next) * v + v_current / nb_next
 
-        Parameters
-        ----------
-        samples_to_evaluate
-            sample to evaluate
-        Returns
-        -------
-        The preprocessed sample to evaluate
-        """
-        evaluate_vect = self.feature_model(samples_to_evaluate[0])
-        return evaluate_vect
+            return i + tf.constant(1, dtype=size.dtype), v_next, nb_next
 
-    def compute_influence_value_from_influence_vector(self, preproc_sample_to_evaluate,
-                                                      influence_vector: tf.Tensor) -> tf.Tensor:
-        """
-        Compute the influence score for a preprocessed sample to evaluate and a training influence vector
+        dtype_ = dataset.element_spec[0].dtype
+        _, influence_vector, __ = tf.while_loop(cond=lambda i, v, nb: i < size,
+                                                        body=body,
+                                                        loop_vars=[tf.constant(0, dtype=size.dtype),
+                                                                   tf.zeros((weight_size,),
+                                                                            dtype=dtype_),
+                                                                   tf.constant(0.0, dtype=dtype_)])
 
-        Parameters
-        ----------
-        preproc_sample_to_evaluate
-            Preprocessed sample to evaluate
-        influence_vector
-            Training influence Vvctor
-        Returns
-        -------
-        The influence score
-        """
-        influence_values = tf.matmul(preproc_sample_to_evaluate, tf.transpose(influence_vector))
-        return influence_values
+        layers_end = model_influence.model.layers[target_layer:]
+        weights = [lay.weights for lay in layers_end]
+        weights = list(itertools.chain(*weights))
 
-    def compute_pairwise_influence_value(self, train_samples: Tuple[tf.Tensor, tf.Tensor]) -> tf.Tensor:
-        """
-        Compute the influence score for a training sample
+        model_end = tf.keras.models.clone_model(Sequential(layers_end))
 
-        Parameters
-        ----------
-        train_samples
-            Training sample
-        Returns
-        -------
-        The influence score
-        """
-        ihvp = self.compute_influence_vector(train_samples)
-        evaluate_vect = self.preprocess_sample_to_evaluate(train_samples)
-        evaluate_vect = tf.cast(evaluate_vect, dtype=ihvp.dtype)
-        influence_values = tf.reduce_sum(
-            tf.math.multiply(evaluate_vect, ihvp), axis=1, keepdims=True)
+        input_layer_shape = model_influence.model.layers[target_layer].input.type_spec.shape
+        model_end.build(input_layer_shape)
+        model_end.set_weights(weights)
 
-        return influence_values
+        self._reshape_assign(model_end.layers[0].weights, influence_vector)
+
+        features_extractor = Sequential(model_influence.model.layers[:target_layer])
+
+        model = InfluenceModel(Sequential([features_extractor, model_end]), 1,
+                               loss_function=model_influence.loss_function)
+
+        ihvp_calculator = ihvp_calculator_factory.build(model, dataset)
+
+        super().__init__(model=model,
+                         dataset=dataset,
+                         ihvp_calculator=ihvp_calculator,
+                         n_samples_for_hessian=n_samples_for_hessian,
+                         shuffle_buffer_size=shuffle_buffer_size,
+                         normalize=False)
+
+    def _reshape_assign(self, weights, influence_vector):
+        index = 0
+        for w in weights:
+            shape = tf.shape(w)
+            size = tf.reduce_prod(shape)
+            v = influence_vector[index:(index + size)]
+            index += size
+            v = tf.reshape(v, shape)
+            w.assign(w - v)
