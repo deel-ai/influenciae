dac7c51b6c839616a3f70976634724db212f053f
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2022-09-30 18:14:00 +0200

    tests: adapt tests to new interface

--- tests/common/test_ihvp.py
+++ tests/common/test_ihvp.py
@@ -39,7 +39,7 @@ def test_exact_hessian():
         tf.expand_dims(hessian_ground_truth(tf.squeeze(inp), kernel), axis=0) for inp in inputs
     ], axis=0)
     ground_truth_inv_hessian = tf.linalg.pinv(tf.reduce_mean(hessian_list, axis=0))
-    assert almost_equal(inv_hessian, ground_truth_inv_hessian)
+    assert almost_equal(inv_hessian, ground_truth_inv_hessian, epsilon=1e-3)
 
 
 def test_exact_ihvp():
@@ -49,14 +49,19 @@ def test_exact_ihvp():
     model.build(input_shape=(1, 3))
     influence_model = InfluenceModel(model, start_layer=-1, loss_function=MeanSquaredError(reduction=Reduction.NONE))
     kernel = tf.reshape(tf.concat([tf.reshape(layer.weights[0], -1) for layer in influence_model.layers], axis=0), -1)
-    inputs = tf.random.normal((5, 1, 3))
-    target = tf.random.normal((5, 1))
+    inputs = tf.random.normal((25, 1, 3))
+    target = tf.random.normal((25, 1))
     train_set = tf.data.Dataset.from_tensor_slices((inputs, target))
 
     # Compute the IHVP using auto-diff and check shapes
     ihvp_calculator = ExactIHVP(influence_model, train_set.batch(5))
     ihvp = ihvp_calculator.compute_ihvp(train_set.batch(5))
-    assert ihvp.shape == (2, 5)  # 5 times (2, 1) stacked on the last axis
+    ihvp_list = []
+    for elt in ihvp:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        ihvp_list.append(elt)
+    ihvp = tf.concat(ihvp_list, axis=1)
+    assert ihvp.shape == (2, 25) # nb_params times nb_elt stacked on the last axis
 
     # Compute the IHVP symbolically and check results
     hessian_list = tf.concat([
@@ -80,17 +85,27 @@ def test_exact_ihvp():
     # test with an initialization with the stochastic_hessian
     ihvp_calculator2 = ExactIHVP(influence_model, train_hessian=stochastic_hessian)
     ihvp2 = ihvp_calculator2.compute_ihvp(train_set.batch(5))
-    assert ihvp2.shape == (2, 5)  # 5 times (2, 1) stacked on the last axis
+    ihvp_list2 = []
+    for elt in ihvp2:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        ihvp_list2.append(elt)
+    ihvp2 = tf.concat(ihvp_list2, axis=1)
+    assert ihvp2.shape == (2, 25)  # nb_params times nb_elt stacked on the last axis
     assert almost_equal(ihvp2, ground_truth_ihvp, epsilon=1e-2)
 
     # Do the same for when the vector is directly provided
-    vectors = tf.random.normal((25, 1, 2))
+    vectors = tf.random.normal((25, 2)) # 25 grads of 2 parameters
     ihvp_vectors = ihvp_calculator.compute_ihvp(
         group=tf.data.Dataset.from_tensor_slices(vectors).batch(5),
         use_gradient=False
     )
-    assert ihvp_vectors.shape == (25, 2, 1)
-    ground_truth_ihvp_vector = tf.matmul(ground_truth_inv_hessian, tf.reshape(vectors, (25, 2, 1)))
+    ihvp_vectors_list = []
+    for elt in ihvp_vectors:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        ihvp_vectors_list.append(elt)
+    ihvp_vectors = tf.concat(ihvp_vectors_list, axis=1)
+    assert ihvp_vectors.shape == (2, 25) # nb_params times nb_elt stacked on the last axis
+    ground_truth_ihvp_vector = tf.matmul(ground_truth_inv_hessian, tf.transpose(vectors))
     assert almost_equal(ihvp_vectors, ground_truth_ihvp_vector, epsilon=1e-3)
 
 
@@ -101,14 +116,19 @@ def test_exact_hvp():
     model.build(input_shape=(1, 3))
     influence_model = InfluenceModel(model, start_layer=-1, loss_function=MeanSquaredError(reduction=Reduction.NONE))
     kernel = tf.reshape(tf.concat([tf.reshape(layer.weights[0], -1) for layer in influence_model.layers], axis=0), -1)
-    inputs = tf.random.normal((5, 1, 3))
-    target = tf.random.normal((5, 1))
+    inputs = tf.random.normal((25, 1, 3))
+    target = tf.random.normal((25, 1))
     train_set = tf.data.Dataset.from_tensor_slices((inputs, target))
 
     # Compute the HVP using auto-diff and check shapes
     hvp_calculator = ExactIHVP(influence_model, train_set.batch(5))
     hvp = hvp_calculator.compute_hvp(train_set.batch(5))
-    assert hvp.shape == (2, 5)  # 5 times (2, 1) stacked on the last axis
+    hvp_list = []
+    for elt in hvp:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        hvp_list.append(elt)
+    hvp = tf.concat(hvp_list, axis=1)
+    assert hvp.shape == (2, 25) # nb_params times nb_elt stacked on the last axis
 
     # Compute the HVP symbolically and check results
     hessian_list = tf.concat([
@@ -122,17 +142,27 @@ def test_exact_hvp():
     # test with an initialization with the stochastic_hessian
     ihvp_calculator2 = ExactIHVP(influence_model, train_hessian=ground_truth_hessian)
     hvp2 = ihvp_calculator2.compute_hvp(train_set.batch(5))
-    assert hvp2.shape == (2, 5)  # 5 times (2, 1) stacked on the last axis
+    hvp_list2 = []
+    for elt in hvp2:
+        assert elt.shape == (2, 5)  # batch_size times (2, 1) stacked on the last axis
+        hvp_list2.append(elt)
+    hvp2 = tf.concat(hvp_list2, axis=1)
+    assert hvp2.shape == (2, 25)  # 5 times (2, 1) stacked on the last axis
     assert almost_equal(hvp2, ground_truth_hvp, epsilon=1e-3)
 
     # Do the same for when the vector is directly provided
-    vectors = tf.random.normal((25, 1, 2))
+    vectors = tf.random.normal((25, 2))
     hvp_vectors = hvp_calculator.compute_hvp(
         group=tf.data.Dataset.from_tensor_slices(vectors).batch(5),
         use_gradient=False
     )
-    assert hvp_vectors.shape == (25, 2, 1)
-    ground_truth_ihvp_vector = tf.matmul(ground_truth_hessian, tf.reshape(vectors, (25, 2, 1)))
+    hvp_vectors_list = []
+    for elt in hvp_vectors:
+        assert elt.shape == (2, 5) # nb_params times batch_size stacked on the last axis
+        hvp_vectors_list.append(elt)
+    hvp_vectors = tf.concat(hvp_vectors_list, axis=1)
+    assert hvp_vectors.shape == (2, 25)
+    ground_truth_ihvp_vector = tf.matmul(ground_truth_hessian, tf.transpose(vectors))
     assert almost_equal(hvp_vectors, ground_truth_ihvp_vector, epsilon=1e-3)
 
 
@@ -143,14 +173,19 @@ def test_cgd_hvp():
     model.build(input_shape=(1, 3))
     influence_model = InfluenceModel(model, start_layer=-1, loss_function=MeanSquaredError(reduction=Reduction.NONE))
     kernel = tf.reshape(tf.concat([tf.reshape(layer.weights[0], -1) for layer in influence_model.layers], axis=0), -1)
-    inputs = tf.random.normal((5, 1, 3))
-    target = tf.random.normal((5, 1))
+    inputs = tf.random.normal((25, 1, 3))
+    target = tf.random.normal((25, 1))
     train_set = tf.data.Dataset.from_tensor_slices((inputs, target))
 
     # Compute the IHVP using auto-diff and check shapes
     ihvp_calculator = ConjugateGradientDescentIHVP(influence_model, extractor_layer=1, train_dataset=train_set.batch(5))
     hvp = ihvp_calculator.compute_hvp(train_set.batch(5))
-    assert hvp.shape == (2, 5)  # 5 times (2, 1) stacked on the last axis
+    hvp_list = []
+    for elt in hvp:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        hvp_list.append(elt)
+    hvp = tf.concat(hvp_list, axis=1)
+    assert hvp.shape == (2, 25) # nb_params times nb_elt stacked on the last axis
 
     # Compute the IHVP symbolically and check results
     hessian_list = tf.concat([
@@ -161,6 +196,20 @@ def test_cgd_hvp():
     ground_truth_hvp = tf.matmul(ground_truth_hessian, ground_truth_grads)
     assert almost_equal(hvp, ground_truth_hvp, epsilon=1e-3)
 
+    # Do the same for when the vector is directly provided
+    vectors = tf.random.normal((25, 2))
+    hvp_vectors = ihvp_calculator.compute_hvp(
+        group=tf.data.Dataset.from_tensor_slices(vectors).batch(5),
+        use_gradient=False
+    )
+    hvp_vectors_list = []
+    for elt in hvp_vectors:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        hvp_vectors_list.append(elt)
+    hvp_vectors = tf.concat(hvp_vectors_list, axis=1)
+    assert hvp_vectors.shape == (2, 25) # nb_params times nb_elt stacked on the last axis
+    ground_truth_hvp_vector = tf.matmul(ground_truth_hessian, tf.transpose(vectors))
+    assert almost_equal(hvp_vectors, ground_truth_hvp_vector, epsilon=1e-3)
 
 def test_cgd_ihvp():
     # Make sure that the shapes are right and that the exact ihvp calculation is correct
@@ -169,14 +218,19 @@ def test_cgd_ihvp():
     model.build(input_shape=(1, 3))
     influence_model = InfluenceModel(model, start_layer=-1, loss_function=MeanSquaredError(reduction=Reduction.NONE))
     kernel = tf.reshape(tf.concat([tf.reshape(layer.weights[0], -1) for layer in influence_model.layers], axis=0), -1)
-    inputs = tf.random.normal((5, 1, 3))
-    target = tf.random.normal((5, 1))
+    inputs = tf.random.normal((25, 1, 3))
+    target = tf.random.normal((25, 1))
     train_set = tf.data.Dataset.from_tensor_slices((inputs, target))
 
     # Compute the IHVP using auto-diff and check shapes
     ihvp_calculator = ConjugateGradientDescentIHVP(influence_model, extractor_layer=-1, train_dataset=train_set.batch(5))
-    hvp = ihvp_calculator.compute_ihvp(train_set.batch(5))
-    assert hvp.shape == (2, 5)  # 5 times (2, 1) stacked on the last axis
+    ihvp = ihvp_calculator.compute_ihvp(train_set.batch(5))
+    ihvp_list = []
+    for elt in ihvp:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        ihvp_list.append(elt)
+    ihvp = tf.concat(ihvp_list, axis=1)
+    assert ihvp.shape == (2, 25) # nb_params times nb_elt stacked on the last axis
 
     # Compute the IHVP symbolically and check results
     hessian_list = tf.concat([
@@ -185,4 +239,19 @@ def test_cgd_ihvp():
     ground_truth_inv_hessian = tf.linalg.pinv(tf.reduce_mean(hessian_list, axis=0))
     ground_truth_grads = tf.concat([jacobian_ground_truth(inp[0], kernel, y) for inp, y in zip(inputs, target)], axis=1)
     ground_truth_ihvp = tf.matmul(ground_truth_inv_hessian, ground_truth_grads)
-    assert almost_equal(hvp, ground_truth_ihvp, epsilon=1e-3)
+    assert almost_equal(ihvp, ground_truth_ihvp, epsilon=1e-3)
+
+    # Do the same for when the vector is directly provided
+    vectors = tf.random.normal((25, 2))
+    ihvp_vectors = ihvp_calculator.compute_ihvp(
+        group=tf.data.Dataset.from_tensor_slices(vectors).batch(5),
+        use_gradient=False
+    )
+    ihvp_vectors_list = []
+    for elt in ihvp_vectors:
+        assert elt.shape == (2, 5)  # nb_params times batch_size stacked on the last axis
+        ihvp_vectors_list.append(elt)
+    ihvp_vectors = tf.concat(ihvp_vectors_list, axis=1)
+    assert ihvp_vectors.shape == (2, 25) # nb_params times nb_elt stacked on the last axis
+    ground_truth_ihvp_vector = tf.matmul(ground_truth_inv_hessian, tf.transpose(vectors))
+    assert almost_equal(ihvp_vectors, ground_truth_ihvp_vector, epsilon=1e-3)
---
+++ tests/common/test_ihvp_factory.py
@@ -0,0 +1,80 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+import pytest
+
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.losses import (Reduction, MeanSquaredError)
+
+from deel.influenciae.common import InfluenceModel
+from deel.influenciae.common import ExactIHVP, ConjugateGradientDescentIHVP
+from deel.influenciae.common import InverseHessianVectorProductFactory, ExactFactory, CGDFactory
+
+from ..utils_test import almost_equal
+
+def test_exact_factory():
+    model = Sequential([Input(shape=(1, 3)), Dense(2, use_bias=False), Dense(1, use_bias=False)])
+    model.build(input_shape=(1, 3))
+    influence_model = InfluenceModel(model, start_layer=-1, loss_function=MeanSquaredError(reduction=Reduction.NONE))
+
+    inputs = tf.random.normal((25, 1, 3))
+    target = tf.random.normal((25, 1))
+    train_set = tf.data.Dataset.from_tensor_slices((inputs, target)).batch(5)
+
+    ihvp = ExactIHVP(influence_model, train_set)
+    exact_factory = ExactFactory()
+    assert isinstance(exact_factory, InverseHessianVectorProductFactory)
+
+    ihvp_from_factory = exact_factory.build(influence_model, train_set)
+    assert isinstance(ihvp_from_factory, ExactIHVP)
+
+    assert almost_equal(ihvp.inv_hessian, ihvp_from_factory.inv_hessian, epsilon=1e-3)
+
+def test_cgd_factory():
+    model = Sequential([Input(shape=(1, 3)), Dense(2, use_bias=False), Dense(1, use_bias=False)])
+    model.build(input_shape=(1, 3))
+    influence_model = InfluenceModel(model, start_layer=-1, loss_function=MeanSquaredError(reduction=Reduction.NONE))
+
+    inputs = tf.random.normal((25, 1, 3))
+    target = tf.random.normal((25, 1))
+    train_set = tf.data.Dataset.from_tensor_slices((inputs, target)).batch(5)
+
+    n_cgd_iters = 100
+
+    # case 1
+    feature_extractor = Sequential(model.layers[:1])
+    ihvp = ConjugateGradientDescentIHVP(influence_model, 1, train_set, n_cgd_iters, feature_extractor)
+    cgd_factory = CGDFactory(feature_extractor, n_cgd_iters, 1)
+    assert isinstance(cgd_factory, InverseHessianVectorProductFactory)
+
+    ihvp_from_factory = cgd_factory.build(influence_model, train_set)
+    assert ihvp.extractor_layer == ihvp_from_factory.extractor_layer
+    assert len(ihvp.feature_extractor.layers) == len(ihvp_from_factory.feature_extractor.layers)
+    assert ihvp.weights == ihvp_from_factory.weights
+    assert ihvp.n_hessian == ihvp_from_factory.n_hessian
+    for ihvp_batch, factory_batch in zip(ihvp.train_set, ihvp_from_factory.train_set):
+        assert almost_equal(ihvp_batch[0], factory_batch[0])
+        assert almost_equal(ihvp_batch[1], factory_batch[1])
+
+    # case 2
+    feature_extractor = 1
+    ihvp = ConjugateGradientDescentIHVP(influence_model, 1, train_set, n_cgd_iters, None)
+    cgd_factory = CGDFactory(feature_extractor, n_cgd_iters)
+    assert isinstance(cgd_factory, InverseHessianVectorProductFactory)
+
+    ihvp_from_factory = cgd_factory.build(influence_model, train_set)
+    assert ihvp.extractor_layer == ihvp_from_factory.extractor_layer
+    assert len(ihvp.feature_extractor.layers) == len(ihvp_from_factory.feature_extractor.layers)
+    assert ihvp.weights == ihvp_from_factory.weights
+    assert ihvp.n_hessian == ihvp_from_factory.n_hessian
+    for ihvp_batch, factory_batch in zip(ihvp.train_set, ihvp_from_factory.train_set):
+        assert almost_equal(ihvp_batch[0], factory_batch[0])
+        assert almost_equal(ihvp_batch[1], factory_batch[1])
+
+    # case 3
+    feature_extractor = Sequential(model.layers[:1])
+    with pytest.raises(AssertionError):
+        cgd_factory = CGDFactory(feature_extractor, n_cgd_iters)
