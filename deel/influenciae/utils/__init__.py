deb56589ead4db8c766c96b98903118d72360ffd
agustin-martin.picard <agustin-martin.picard@irt-saintexupery.com>
2022-11-03 11:36:09 +0100

    sort order: add import to init

--- deel/influenciae/utils/__init__.py
+++ deel/influenciae/utils/__init__.py
@@ -8,6 +8,6 @@ Utility classes and functions
 from .conjugate_gradients import conjugate_gradients_solve
 from .backtracking_line_search import BacktrackingLineSearch
 from .tf_operations import find_layer, from_layer_name_to_layer_idx, is_dataset_batched, \
-     assert_batched_dataset, dataset_size, default_process_batch
-from .sorted_dict import BatchSort
+     assert_batched_dataset, dataset_size, default_process_batch, dataset_to_tensor, array_to_dataset
+from .sorted_dict import BatchSort, ORDER
 from .nearest_neighbors import BaseNearestNeighbors, LinearNearestNeighbors
