889dd68cf54706d6d2a8f4e66ec7595cb5f1b19f
agustin-martin.picard <agustin-martin.picard@irt-saintexupery.com>
2023-02-08 10:44:16 +0100

    boundary_based: add two influence methods based on the distance to the decision boundary

---
+++ deel/influenciae/boundary_based/__init__.py
@@ -0,0 +1,10 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+"""
+Module containing methods based on decision boundaries and adversarial attacks for
+estimating the influence of data-points.
+"""
+from .weights_boundary import WeightsBoundaryCalculator
+from .sample_boundary import SampleBoundaryCalculator
