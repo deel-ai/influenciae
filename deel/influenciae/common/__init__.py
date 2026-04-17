60309ef52cfb4cf89832836eb456c54569ba4b08
Agustin Picard <agustin.picard@gmail.com>
2026-04-17 21:08:53 +0200

    object_detection: extend lazy import to heavy modules

--- deel/influenciae/common/__init__.py
+++ deel/influenciae/common/__init__.py
@@ -6,6 +6,8 @@
 Common classes and methods
 """
 
+import importlib
+
 from .backend import (
     Framework,
     BaseBackend,
@@ -24,9 +26,43 @@ from .model_wrappers import (
     PyTorchInfluenceModel,
     default_process_batch,
 )
-from .base_influence import SelfInfluenceCalculator, BaseInfluenceCalculator, CACHE
-from .inverse_hessian_vector_product import InverseHessianVectorProduct, ExactIHVP, ConjugateGradientDescentIHVP, \
-     IHVPCalculator, LissaIHVP, ForwardOverBackwardHVP, KfacIHVP, EkfacIHVP
-from .ihvp_factory import InverseHessianVectorProductFactory, ExactIHVPFactory, CGDIHVPFactory, LissaIHVPFactory, \
-     KfacIHVPFactory, EkfacIHVPFactory
-from .kfac_factors import LayerParameterMap, KroneckerFactors, EKFACFactors
+from .evaluation import EvaluationRepresentationProvider, ObjectiveEvaluationRepresentationProvider
+from .payloads import TrainingPayloadExtractor, default_training_payload_extractor
+
+_LAZY_ATTRS = {
+    "SelfInfluenceCalculator": ".base_influence",
+    "BaseInfluenceCalculator": ".base_influence",
+    "CACHE": ".base_influence",
+    "InverseHessianVectorProduct": ".inverse_hessian_vector_product",
+    "ExactIHVP": ".inverse_hessian_vector_product",
+    "ConjugateGradientDescentIHVP": ".inverse_hessian_vector_product",
+    "IHVPCalculator": ".inverse_hessian_vector_product",
+    "LissaIHVP": ".inverse_hessian_vector_product",
+    "ForwardOverBackwardHVP": ".inverse_hessian_vector_product",
+    "KfacIHVP": ".inverse_hessian_vector_product",
+    "EkfacIHVP": ".inverse_hessian_vector_product",
+    "InverseHessianVectorProductFactory": ".ihvp_factory",
+    "ExactIHVPFactory": ".ihvp_factory",
+    "CGDIHVPFactory": ".ihvp_factory",
+    "LissaIHVPFactory": ".ihvp_factory",
+    "KfacIHVPFactory": ".ihvp_factory",
+    "EkfacIHVPFactory": ".ihvp_factory",
+    "LayerParameterMap": ".kfac_factors",
+    "KroneckerFactors": ".kfac_factors",
+    "EKFACFactors": ".kfac_factors",
+}
+
+
+def __getattr__(name):
+    """Lazily import heavier common modules to avoid circular imports."""
+    if name not in _LAZY_ATTRS:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+    module = importlib.import_module(_LAZY_ATTRS[name], __name__)
+    value = getattr(module, name)
+    globals()[name] = value
+    return value
+
+
+def __dir__():
+    """Expose lazily loaded attributes in interactive environments."""
+    return sorted(list(globals().keys()) + list(_LAZY_ATTRS.keys()))
